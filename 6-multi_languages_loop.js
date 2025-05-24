bcbbb606cff100e07eea9971324a52d010963af9
babasalieu360 <jallowbaba310@mail.ru>
2025-05-24 18:02:04 +0000

    6 multi languages loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,1 +1,4 @@
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) {
+  console.log(line);
+}
